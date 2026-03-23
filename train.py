4fdee7772bd5a488c8160b3d7a5138f51030194d
Lxx1735827 <870375416@qq.com>
2026-03-23 14:03:31 +0800

    sam2

--- train.py
+++ train.py
@@ -240,7 +240,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
         rgb_loss = (1.0 - opt.lambda_dssim) * Ll1_render + opt.lambda_dssim * (1.0 - ssim(rendered_image, gt_image.unsqueeze(0)))
 
         if iteration > opt.iterations * 0.5:
-            loss = rgb_loss + 0.1 * pcc_depth_loss + 0.1 * depth_order_loss
+            loss = rgb_loss + 0.1 * pcc_depth_loss + 0.05 * depth_order_loss
         else:
             loss = rgb_loss
         loss.backward()
